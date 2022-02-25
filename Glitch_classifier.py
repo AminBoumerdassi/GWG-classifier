19e144bb79e0d487413bcabff93eebbb840a6dc5
AminBoumerdassi <99902066+AminBoumerdassi@users.noreply.github.com>
2022-02-25 00:11:25 +0000

    Update Glitch_classifier.py
    Restructured this script to relocate label generation and dataset formatting. This is to avoid memory errors.

--- Glitch_classifier.py
+++ Glitch_classifier.py
@@ -3,57 +3,35 @@
 Created on Mon Feb 21 14:58:08 2022
 
 @author: Amin Boumerdassi
+
+This script contains the CNN and trains it on glitch data and
+pre-encoded labels. To run this, first run
+create_glitch_array.py and create_glitch_labels.py.
 """
 #Import relevant modules
 from tensorflow import keras
 from tensorflow.keras.layers import Conv1D, MaxPooling1D, Dropout, Flatten, Dense
-from tensorflow.keras.utils import to_categorical
-from numpy import loadtxt, array, append, shape
-from sklearn.preprocessing import LabelEncoder
-import glob
-from downsampler import downsample
+from numpy import shape, load
 from sklearn.model_selection import train_test_split
 
+#Load O1+2 glitch data from .npy
+dsampled_glitch_data= load("glitch_data_4096_pts.npy")
 
-#Import glitch data
-glitch_directory = 'C:\\Users\\aminb\\Desktop\\4th year project\\Glitch files\\o1_text_files\\'
+#Load pre-encoded y labels from .npy
+ylabel= load("glitch_labels_encoded.npy")
 
-#Locating a small number of glitches for debugging
-glitch_files= glob.glob(glitch_directory+'Tomte'+ '*.txt')+glob.glob(glitch_directory+'Whistle'+ '*.txt')+glob.glob(glitch_directory+'Air_Compressor'+'*.txt')
-
-glitch_data= array([loadtxt(glitch) for glitch in glitch_files])
-
-#May be useful to normalise the data in some way e.g. divide by 1e-21
-
-#Downsample the data
-npts=5000
-dsampled_glitch_data= array([downsample(i,npts) for i in glitch_data])
-
-#Define training labels
-classes= array(['Air_Compressor', 'Blip', 'Extremely_Loud', 'Koi_Fish',
-       'Low_Frequency_Burst', 'Low_Frequency_Lines', 'Power_Line',
-       'Repeating_Blips', 'Scattered_Light', 'Scratchy', 'Tomte', 'Whistle'])
-glitch_labels= array([])
-
-for i in glitch_files:
-    glitch_labels= append(glitch_labels,[label for label in classes if label in i])
-    
-#Encoding labels
-num=3#No. of unique classes as *loaded*. May be less than len(classes)
-label_encoder= LabelEncoder()
-classes_encoded= label_encoder.fit_transform(glitch_labels)
-ylabel= to_categorical(classes_encoded, num_classes=num)    
-    
+#May be useful to normalise the data in some way e.g. divide by 1e-21    
     
 #Reshape x data to fit into the CNN (labels already in the right dimensions)
-dsampled_glitch_data= dsampled_glitch_data.reshape(len(dsampled_glitch_data),npts,-1)
+dimensions= shape(dsampled_glitch_data)
+dsampled_glitch_data= dsampled_glitch_data.reshape(dimensions[0],dimensions[1],-1)
 
 #Creating test-train split
 X_train, X_test, y_train, y_test = train_test_split(dsampled_glitch_data, ylabel, test_size=0.33, random_state=38)
 
 #Defining the CNN
 model = keras.Sequential()
-model.add(Conv1D(filters=128, kernel_size=3, activation='relu',input_shape=(npts,1)))
+model.add(Conv1D(filters=128, kernel_size=3, activation='relu',input_shape=(dimensions[1],1)))
 model.add(Dropout(0.3))
 model.add(MaxPooling1D(pool_size=2))
 model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
@@ -62,14 +40,13 @@ model.add(Conv1D(filters=64, kernel_size=3, activation='relu'))
 model.add(MaxPooling1D(pool_size=2))
 model.add(Flatten())
 model.add(Dense(100, activation='relu'))
-model.add(Dense(num, activation='softmax'))#no. of nodes depends on no. of *used* classes
+model.add(Dense(shape(ylabel)[1], activation='softmax'))#no. of nodes depends on no. of *used* classes  
 model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adadelta(
     learning_rate=1.0, rho=0.95, epsilon=1e-07, name="Adadelta"), metrics=['accuracy'])
 
 #Fitting the CNN
-history = model.fit(X_train, y_train, epochs=20, batch_size=7, verbose=1, validation_data=(X_test, y_test))#Dimension issues on this line
+history = model.fit(X_train, y_train, epochs=20, batch_size=25, verbose=1, validation_data=(X_test, y_test))
 
 #Evaluate CNN performance
 _, accuracy = model.evaluate(X_test, y_test, batch_size=64, verbose=1)
 print("Validation accuracy: {}".format(accuracy))
-
