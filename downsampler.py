8c25087aa970460b1ce10db33c414c06852e0e5e
AminBoumerdassi <99902066+AminBoumerdassi@users.noreply.github.com>
2022-03-02 01:27:19 +0000

    Update downsampler.py
    Removed redundant variable storage.

--- downsampler.py
+++ downsampler.py
@@ -2,13 +2,11 @@
 """
 Created on Mon Feb 21 16:17:49 2022
 
-@author: Amin
+@author: Amin Boumerdassi
 """
 from scipy.interpolate import interp1d
 from numpy import arange, linspace
 
-
 def downsample(array, npts):#Downsamples an array to some no. of elements "npts"
     interpolated = interp1d(arange(len(array)), array, axis = 0, fill_value = 'extrapolate')
-    downsampled = interpolated(linspace(0, len(array), npts))
-    return downsampled
+    return interpolated(linspace(0, len(array), npts))
